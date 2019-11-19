c701d361947adedde63f3fbed2bb63b74277270a
Florian Porada <florian.porada@gmail.com>
2019-11-19 23:36:05 +0100

    Update bridge and py printer script

--- src/client/components/bridge.js
+++ src/client/components/bridge.js
@@ -1,30 +1,29 @@
 import path from 'path';
 import { PythonShell } from 'python-shell';
 
-const pyshell = new PythonShell(path.normalize(path.join(__dirname, '..', 'python', 'print.py')));
+const pyshell = new PythonShell(path.normalize(path.join(__dirname, '..', 'python', 'print.py')), {
+  pythonPath: '/usr/bin/python'
+});
 
 /**
- *
+ * Sends the data to the python script which handles the communication with the printer
  *
  * @param {Object} data
  * @param {string} data.message - message which will be sent to the printer
  * @param {string} data.image - base64 encoded image string
- * @param {string} data.from - the name of the sender
+ * @param {string} data.sender - the name of the sender
  */
 const sendToPrintScript = data => {
   return new Promise((resolve, reject) => {
-    pyshell.send(data);
+    pyshell.send(JSON.stringify(data));
 
-    pyshell.end(function(err, code, signal) {
+    pyshell.end(function(err, code) {
       if (err)
         reject({
           message: 'Error while executing the python script',
+          code,
           err
         });
-
-      console.log('The exit code was: ' + code);
-      console.log('The exit signal was: ' + signal);
-      console.log('finished');
 
       resolve({
         message: 'Python script executed successfully',
--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,8 @@
 import io from 'socket.io-client';
+import express from 'express';
 
-import { sendToPrintScript } from './components/bridge';
+// import { sendToPrintScript } from './components/bridge';
+import logger from '../lib/logger';
 
 /**
  *
@@ -12,15 +14,25 @@ class PrinterClient {
     this.url = props.url || 'http://localhost:3030';
     this.name = props.name || 'Printy McPrintface';
     this.uid = props.uid || 0;
+    this.webserverPort = props.webserverPort || 8080;
   }
 
-  init() {
+  initSocket() {
     const socket = io.connect(this.url);
-    socket.emit('register_printer', { type: 'printer', name: this.name, uid: this.uid });
+    socket.emit('register_printer', {
+      type: 'printer',
+      name: this.name,
+      uid: this.uid
+    });
 
     socket.on('print_message', async (data, cb) => {
+      console.log(data);
+
       try {
-        const res = await sendToPrintScript({ message: data.message, from: data.sender });
+        const res = await sendToPrintScript({
+          message: data.message,
+          sender: data.sender
+        });
 
         cb({
           from: this.uid,
@@ -35,6 +47,29 @@ class PrinterClient {
         });
       }
     });
+  }
+
+  initWeb() {
+    const app = express();
+
+    app.use('/', express.static(`${__dirname}/public`));
+
+    app.get('/config', (req, res) => {
+      res.send({});
+    });
+
+    app.put('/config', (req, res) => {
+      res.send({ message: 'saved config to db' });
+    });
+
+    app.listen(this.webserverPort, () => {
+      logger.info(`Starting webserver on http://localhost:${this.webserverPort}`);
+    });
+  }
+
+  init() {
+    this.initSocket();
+    this.initWeb();
   }
 }
 
