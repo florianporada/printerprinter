1f818d17dc22a30bdde6812aaa5fd7a3716436d2
Florian Porada <florian.porada@gmail.com>
2019-11-20 16:04:12 +0100

    Update pythonshell init process

--- src/client/components/bridge.js
+++ src/client/components/bridge.js
@@ -1,15 +1,47 @@
 import path from 'path';
 import { PythonShell } from 'python-shell';
+import fs from 'fs';
+import logger from '../../lib/logger';
 
-const pyshell = new PythonShell(path.normalize(path.join(__dirname, '..', 'python', 'print.py')), {
-  pythonPath: '/usr/bin/python'
-});
+const dbPath = path.join(__dirname, '..', 'db.json');
+let pyshell;
+
+/**
+ * Script for initializing the python print script
+ *
+ */
+function initShell() {
+  try {
+    const { config } = JSON.parse(fs.readFileSync(dbPath));
+
+    if (!config.baudrate && !config.serialport) {
+      logger.info('No valid config found. Try again in 5 seconds');
+      setTimeout(() => {
+        initShell();
+      }, 5000);
+    }
+
+    pyshell = new PythonShell(path.normalize(path.join(__dirname, '..', 'python', 'print.py')), {
+      pythonPath: '/usr/bin/python',
+      args: [
+        '--serialport',
+        config.serialport || '/dev/ttyS0',
+        '--baudrate',
+        config.baudrate || 9600
+      ]
+    });
+  } catch (error) {
+    logger.error(`could not init python shell, try again in 5 seconds`);
+  }
+}
+
+initShell();
 
 /**
  * Sends the data to the python script which handles the communication with the printer
  *
  * @param {Object} data
- * @param {string} data.message - message which will be sent to the printer
+ * @param {string} data.text - message which will be sent to the printer
  * @param {string} data.image - base64 encoded image string
  * @param {string} data.sender - the name of the sender
  */
