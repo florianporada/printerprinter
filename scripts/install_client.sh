9233acac07a0ce337d45618ee0a79e3e98fd6546
Florian Porada <florian.porada@gmail.com>
2020-05-22 21:52:13 +0200

    chore: update install script with permissions

--- scripts/install_client.sh
+++ scripts/install_client.sh
@@ -16,7 +16,7 @@ logthis 'Install nodejs lts'
 wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v.lts.sh | bash
 
 logthis 'Install python packages via PIP'
-pip --no-cache-dir install Pillow
+sudo pip --no-cache-dir install Pillow pyserial
 
 logthis 'Please activate GPIO serial port'
 logthis 'see: https://www.abelectronics.co.uk/kb/article/1035/raspberry-pi-3--4-and-zero-w-serial-port-usage'
@@ -36,12 +36,12 @@ echo 'root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline fsck.repair=yes root
 logthis 'Install PM2 + Logrotate'
 npm install -g pm2
 sudo ln -s /opt/nodejs/bin/pm2 /usr/bin/pm2
-pm2 install pm2-logrotate
+sudo pm2 install pm2-logrotate
 
 logthis 'Configure PM2 Logrotate'
-pm2 set pm2-logrotate:max_size 20M
-pm2 set pm2-logrotate:compress true
-pm2 set pm2-logrotate:rotateInterval '0 0 * * *'
+sudo pm2 set pm2-logrotate:max_size 20M
+sudo pm2 set pm2-logrotate:compress true
+sudo pm2 set pm2-logrotate:rotateInterval '0 0 * * *'
 
 logthis 'Install printerprinter client'
 git clone https://github.com/florianporada/printerprinter.git
@@ -50,9 +50,9 @@ npm install
 npm build
 
 logthis 'Configure client to run on startup'
-pm2 start ./src/client/ecosystem.config.yaml
-pm2 startup
-pm2 save
+sudo pm2 start ./src/client/ecosystem.config.yaml
+sudo pm2 startup
+sudo pm2 save
 
 logthis 'Finished installed script. Visit http://<ip-of-client>:8080/'
 
