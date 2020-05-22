9210aa97836d58fd72683e25c0513e6c24bb716b
Florian Porada <florian.porada@gmail.com>
2020-05-22 19:51:13 +0200

    fix: install script for client

--- scripts/install_client.sh
+++ scripts/install_client.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 logthis() {
   now=$(date +"%T")
   name=PRINTERPRINTER
@@ -10,14 +9,14 @@ logthis() {
 logthis 'Get updates'
 sudo apt-get update -y
 
-eclogthisho 'Install needed packages via apt-get'
-sudo apt-get install -y git build-essentials wget python-pip
+logthis 'Install needed packages via apt-get'
+sudo apt-get install -y git build-essential wget python-pip libjpeg-dev zlib1g-dev
 
 logthis 'Install nodejs lts'
 wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v.lts.sh | bash
 
 logthis 'Install python packages via PIP'
-pip install Pillow
+pip --no-cache-dir install Pillow
 
 logthis 'Please activate GPIO serial port'
 logthis 'see: https://www.abelectronics.co.uk/kb/article/1035/raspberry-pi-3--4-and-zero-w-serial-port-usage'
@@ -27,6 +26,7 @@ logthis 'DISABLE for shell access'
 logthis 'ENABLE as serial port'
 
 read -n 1 -s -r -p "Press any key to continue"
+sleep 5000
 
 sudo raspi-config
 
@@ -34,7 +34,8 @@ logthis 'Modify /boot/cmdline.txt'
 echo 'root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait' | sudo tee /boot/cmdline.txt
 
 logthis 'Install PM2 + Logrotate'
-sudo npm install -g pm2
+npm install -g pm2
+sudo ln -s /opt/nodejs/bin/pm2 /usr/bin/pm2
 pm2 install pm2-logrotate
 
 logthis 'Configure PM2 Logrotate'
@@ -42,6 +43,16 @@ pm2 set pm2-logrotate:max_size 20M
 pm2 set pm2-logrotate:compress true
 pm2 set pm2-logrotate:rotateInterval '0 0 * * *'
 
-logthis 'Reboot'
-sudo reboot
+logthis 'Install printerprinter client'
+git clone https://github.com/florianporada/printerprinter.git
+cd printerprinter
+npm install
+npm build
+
+logthis 'Configure client to run on startup'
+pm2 start ./src/client/ecosystem.config.yaml
+pm2 startup
+pm2 save
+
+logthis 'Finished installed script. Visit http://<ip-of-client>:8080/'
 
