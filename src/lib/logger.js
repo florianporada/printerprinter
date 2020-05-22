3b08ad38298cb539a83defd4ee0c5a10256374e9
Florian Porada <florian.porada@gmail.com>
2020-05-22 22:55:19 +0200

    feat: update logger format

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,8 +1,18 @@
 import winston from 'winston';
 
+const custom = winston.format.printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
 const logger = winston.createLogger({
-  format: winston.format.combine(winston.format.timestamp(), winston.format.json(), winston.format.prettyPrint()),
-  transports: [new winston.transports.Console()]
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.json(),
+    winston.format.prettyPrint(),
+    custom
+  ),
+  transports: [new winston.transports.Console()],
 });
 
 export default logger;
