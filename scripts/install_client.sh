98f733ea4a0f27d45e947cb47a664ed665f3843b
Florian Porada <florian.porada@gmail.com>
2020-05-22 18:18:39 +0200

    chore: update install script for client

--- scripts/install_client.sh
+++ scripts/install_client.sh
@@ -1,30 +1,47 @@
 #!/bin/bash
 
-echo 'Get updates'
+
+logthis() {
+  now=$(date +"%T")
+  name=PRINTERPRINTER
+  echo "$name ($now): $1"
+}
+
+logthis 'Get updates'
 sudo apt-get update -y
 
-echo 'Install needed packages via apt-get'
+eclogthisho 'Install needed packages via apt-get'
 sudo apt-get install -y git build-essentials wget python-pip
 
-echo 'Install nodejs lts'
+logthis 'Install nodejs lts'
 wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v.lts.sh | bash
 
-echo 'Install python packages via PIP'
+logthis 'Install python packages via PIP'
 pip install Pillow
 
-echo 'Please activate GPIO serial port'
-echo 'see: https://www.abelectronics.co.uk/kb/article/1035/raspberry-pi-3--4-and-zero-w-serial-port-usage'
-echo 'Select: 5 Interfacing Options'
-echo 'Select: P6 Serial'
-echo 'DISABLE for shell access'
-echo 'ENABLE as serial port'
+logthis 'Please activate GPIO serial port'
+logthis 'see: https://www.abelectronics.co.uk/kb/article/1035/raspberry-pi-3--4-and-zero-w-serial-port-usage'
+logthis 'Select: 5 Interfacing Options'
+logthis 'Select: P6 Serial'
+logthis 'DISABLE for shell access'
+logthis 'ENABLE as serial port'
 
 read -n 1 -s -r -p "Press any key to continue"
 
 sudo raspi-config
 
-echo 'Modify /boot/cmdline.txt'
+logthis 'Modify /boot/cmdline.txt'
 echo 'root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait' | sudo tee /boot/cmdline.txt
 
+logthis 'Install PM2 + Logrotate'
+sudo npm install -g pm2
+pm2 install pm2-logrotate
+
+logthis 'Configure PM2 Logrotate'
+pm2 set pm2-logrotate:max_size 20M
+pm2 set pm2-logrotate:compress true
+pm2 set pm2-logrotate:rotateInterval '0 0 * * *'
+
+logthis 'Reboot'
 sudo reboot
 
